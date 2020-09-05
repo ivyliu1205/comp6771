0319e6d2ad735dd082a59e541b91a4f729c8dd0b
Yiting Liu <z5211008@ad.unsw.edu.au>
2020-09-05 15:51:26 +1000

    t2 finished

---
+++ Assignment/vcpkg-export-20200721-161758/installed/x64-linux-libcxx/include/gsl-lite.hpp
@@ -0,0 +1,5 @@
+#ifndef GSL_LITE_HPP_VCPKG_COMPAT_HEADER_INCLUDED
+#define GSL_LITE_HPP_VCPKG_COMPAT_HEADER_INCLUDED
+#pragma message("The header <gsl-lite.hpp> is deprecated and provided only for compatibility; please include <gsl/gsl-lite.hpp> instead.")
+#include <gsl/gsl-lite.hpp>
+#endif // GSL_LITE_HPP_VCPKG_COMPAT_HEADER_INCLUDED
